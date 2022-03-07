bed416f31a2f27529008d7c3d8506380a1fb3651
cat32tw <mike1993xd@gmail.com>
2022-03-07 23:44:30 +0800

    第八章ComboBox練習

---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nightMarket
+{
+    public partial class Form1 : Form
+    {
+
+        string[] foodItems = new string[] { "章魚燒", "香酥臭豆腐", "豬米血糕", "胡椒餅" };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.AddRange(foodItems);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!comboBox1.Items.Contains(comboBox1.Text))
+            {
+                comboBox1.Items.Add(comboBox1.Text);
+            }
+            else
+            {
+                return;
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.RemoveAt(comboBox1.SelectedIndex);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            comboBox1.Text = "";
+            comboBox1.Items.Clear();
+        }
+
+        
+    }
+}
